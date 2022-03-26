34a936f0b582f73d6b3c07fcd93c66fe6688466d
Luisa26olivera <101460364+Luisa26olivera@users.noreply.github.com>
2022-03-26 07:23:32 -0500

    Add files via upload

---
+++ Capitulo 4/tarea1.py
@@ -0,0 +1,34 @@
+descuento=float(input("digite el porcentaje de descuento:  "))
+valor_superior=float(input("digite el valor:  "))
+color_balota=(input("digite el color de la balota:  "))
+
+descuento=valor_superior*descuento
+L=descuento-valor_superior
+
+if color_balota== "roja":
+    valor_superior>=50.000
+    descuento == valor_superior*descuento
+    L== descuento- valor_superior
+    print (f"valor superior={valor_superior}\n valor del descuento={descuento}\nvalor total del descuento={L}")
+elif color_balota== "verde":
+    valor_superior>=50.000
+    descuento == valor_superior*descuento
+    L== descuento- valor_superior
+    print (f"valor superior={valor_superior}\n valor del descuento={descuento}\nvalor total del descuento={L}")
+elif color_balota== "blanca":
+    valor_superior>=50.000
+    descuento == valor_superior*descuento
+    L== descuento- valor_superior
+    print (f"valor superior={valor_superior}\n valor del descuento={descuento}\nvalor total del descuento={L}")
+elif color_balota== "negra":
+    valor_superior>=50.000
+    descuento == valor_superior*descuento
+    L== descuento- valor_superior
+    print (f"valor superior={valor_superior}\n valor del descuento={descuento}\nvalor total del descuento={L}")
+elif color_balota== "amarilla":
+    valor_superior>=50.000
+    descuento == valor_superior*descuento
+    L== descuento- valor_superior
+    print (f"valor superior={valor_superior}\n valor del descuento={descuento}\nvalor total del descuento={L}")
+else:
+    print("NO aplica el descuento y el valor a pagar")
