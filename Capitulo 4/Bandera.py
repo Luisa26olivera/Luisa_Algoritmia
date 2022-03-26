d4eb14d7057c7f63fa436b596825f88983dce82c
Luisa26olivera <101460364+Luisa26olivera@users.noreply.github.com>
2022-03-26 08:50:02 -0500

    Add files via upload
    Actualizacion1

---
+++ Capitulo 4/Bandera.py
@@ -0,0 +1,12 @@
+Pais=(input("escriba el nombre del Pais:   "))
+if Pais=="Italia":
+    bandera=("💙")
+    print(f"bandera:{bandera}    ")
+elif Pais=="Brasil":
+    bandera=("💚")
+    print(f"bandera:{bandera}  ")
+elif Pais=="Colombia":
+    bandera=("💛")
+    print(f"bandera:{bandera} ")
+else:
+    print(f"la bandera no se encontro")
